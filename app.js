8dd2a65f20d08a885d589beffb86d6355850f80d
lgants <lgants@gmail.com>
2017-07-08 12:10:12 -0500

    setup project skeleton

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+// use the Express.static middleware to get Express to serve all the static files in the directory /public
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+// matching pattern is the route specified when the module is imported (i.e. '/users') plus whatever is defined in this file (i.e. '/')
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
